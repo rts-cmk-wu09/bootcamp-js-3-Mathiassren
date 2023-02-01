0c8d10fcb308e6e8624ee3e9ff745f18c16bc8ad
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2023-02-01 15:15:57 +0100

    Added JS

--- prevent-load.js
+++ prevent-load.js
@@ -1,24 +1,14 @@
-window.addEventListener('load', function() {
-  // Henter link i linkcontainer. Dette gøres ud fra ClassName
-  var linkContainer = document.getElementsByClassName('primaryNavigation')[0];
+const main = document.querySelector("main");
+const links = document.querySelectorAll(".primaryNavigation__navigationItem a");
+const pages = ['page1.html', 'page2.html', 'page3.html'];
 
-  // henter alle links i link containeren.
-  var links = linkContainer.getElementsByTagName('a');
-
-  // Looper links
-  for (var i = 0; i < links.length; i++) {
-    // Tager det eksisterende link
-    var link = links[i];
-
-    // Tilføjer søgeparameter
-    link.href = link.href + '?t=' + Date.now();
-  }
+links.forEach((link, index) => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    fetch(pages[index])
+      .then(res => res.text())
+      .then(html => {
+        main.innerHTML = html;
+      });
+  });
 });
-
-/*Hvad er søgeparametre (Search Parameters) Og hvorfor bruger vi det? 
-
-Du kan bruge søgeparametre i en URL for at forhindre forud indlæsning af ressourcer af browseren. 
-Ved at tilføje en unik parameter til URL'en bliver det en ny URL, 
-som browseren ikke har set før, og derfor vil den ikke forud indlæse. 
-Denne teknik kan være nyttig i situationer, hvor du vil forhindre browseren i at gemme en ressource, 
-som du vil have skal genereres dynamisk hver gang, det anmodes om. */
